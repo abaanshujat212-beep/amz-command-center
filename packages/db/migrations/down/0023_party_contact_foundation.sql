ee8cf24c65812206f68e0afe8b0bcde50a38c769
Abaan Shujaat <abaanshujat212@gmail.com>
2026-09-14 17:05:46 +0500

    feat: add tenant-safe party and contact foundation

---
+++ packages/db/migrations/down/0023_party_contact_foundation.sql
@@ -0,0 +1,8 @@
+drop table if exists party_merge_history;
+drop table if exists party_relationship;
+drop table if exists party_address;
+drop table if exists contact_channel;
+drop table if exists contact;
+drop table if exists party_role;
+drop table if exists party;
+drop function if exists party_sensitive_allowed();
---
+++ services/party/__init__.py
@@ -0,0 +1,1 @@
+"""Tenant-safe party and contact domain."""
