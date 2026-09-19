93e74cf565145234353b7fee3011f07bd3485e75
Abaan Shujaat <abaanshujat212@gmail.com>
2026-09-19 12:32:34 +0500

    feat: add notification provider readiness rollback

---
+++ packages/db/migrations/down/0038_notification_provider_readiness.sql
@@ -0,0 +1,4 @@
+drop trigger if exists notification_provider_state_audit on notification_provider_state;
+drop function if exists audit_notification_provider_state();
+drop table if exists notification_provider_state_audit;
+drop table if exists notification_provider_state;
