0ed3e5fd0c437517f5311720cb1391b6ab748224
Abaan Shujaat <abaanshujat212@gmail.com>
2026-09-19 11:23:30 +0500

    feat: add consent ledger rollback migration

---
+++ packages/db/migrations/down/0037_notification_consent_ledger.sql
@@ -0,0 +1,4 @@
+drop trigger if exists notification_consent_event_immutable on notification_consent_event;
+drop function if exists protect_notification_consent_event();
+drop function if exists notification_consent_effective(uuid, text, text, text);
+drop table if exists notification_consent_event;
