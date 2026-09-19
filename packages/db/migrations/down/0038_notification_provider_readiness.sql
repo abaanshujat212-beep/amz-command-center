216f2fa052f07a8af5e6fa1c9c02ae4ba2ffc5b4
Abaan Shujaat <abaanshujat212@gmail.com>
2026-09-19 12:34:04 +0500

    fix: complete provider readiness rollback

--- packages/db/migrations/down/0038_notification_provider_readiness.sql
+++ packages/db/migrations/down/0038_notification_provider_readiness.sql
@@ -1,4 +1,6 @@
+drop trigger if exists notification_provider_state_audit_immutable on notification_provider_state_audit;
 drop trigger if exists notification_provider_state_audit on notification_provider_state;
+drop function if exists protect_notification_provider_audit();
 drop function if exists audit_notification_provider_state();
 drop table if exists notification_provider_state_audit;
 drop table if exists notification_provider_state;
